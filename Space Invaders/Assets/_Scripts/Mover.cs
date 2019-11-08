53bc1ae7f4a2e99ae9c86050d83c9d00fba092cd
awybin <awybin@gmail.com>
2019-11-08 18:27:57 -0300

    começo

---
+++ Space Invaders/Assets/_Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speedU;
+	public float speedF;
+	void Start (){
+		rb = GetComponent<Rigidbody>();
+		if (speedU == 0) {
+			rb.velocity = transform.forward * speedF;
+		}
+		if (speedF == 0) {
+			rb.velocity = transform.up * speedU;
+		}
+	}
+}
